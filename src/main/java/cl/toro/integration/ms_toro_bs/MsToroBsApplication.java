5a259192f1017740f541cc6f59b4665eb43f7d4b
ToroDanger <bryanpmg90@gmail.com>
2025-04-08 20:06:19 -0400

    first commit

---
+++ src/main/java/cl/toro/integration/ms_toro_bs/MsToroBsApplication.java
@@ -0,0 +1,13 @@
+package cl.toro.integration.ms_toro_bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsToroBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsToroBsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/toro/integration/ms_toro_bs/MsToroBsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.toro.integration.ms_toro_bs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsToroBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
